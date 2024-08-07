0e3cb92b7cfe3278a65de16736a08a14082cd60b
tatchapong <specter15564@gmail.com>
2024-08-07 19:59:26 +0700

    make signin page

--- src/components/BottomHeader.js
+++ src/components/BottomHeader.js
@@ -5,12 +5,12 @@ const BottomHeader = () => {
   return (
     <header className={styles.header}>
         <div className={styles.buttonWrapper}>
-          <a href="/homepage" className={styles.buttonhome}>home</a>
+          <a href="/homepage" className={styles.buttonhome}>hhh</a>
           <img src='/homewhite.svg' alt='Home White' className={styles.homewhite}/>
           <img src='/purplebutton.svg' alt='Purple Button' className={styles.purplebutton}/>
         </div>
         <div className={styles.buttonWrapper1}>
-          <a href="/search" className={styles.buttonsearch}>search</a>
+          <a href="/search" className={styles.buttonsearch}>sss</a>
           <img src='/searchblack.svg' alt='Search Black' className={styles.searchblack}/>
         </div>
         <div className={styles.buttonWrapper2}>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import styles from '../styles/SignIn.module.css';
+import Header from './Header';
+import BottomHeader from './BottomHeader';
 
 const SignIn = () => {
   return (
     <div className={styles.signInContainer}>
-      <header></header>
-      <h2>Sign In</h2>
+      <Header />
+      <BottomHeader />
+      <div className={styles.topic}>Sign in</div>
+      <div className={styles.buttonback}>
+        <a href="/homepage" className={styles.back}>O</a>
+        <img src='' alt='Back' className={styles.iconback}/>      
+      </div>
       <form>
         <div className={styles.formGroup}>
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">email/username</label>
           <input type="email" id="email" name="email" required />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password">password</label>
           <input type="password" id="password" name="password" required />
         </div>
-        <button type="submit" className={styles.submitButton}>Sign In</button>
+        <div className={styles.buttonconfirm}>
+          <a href="/homepage" className={styles.confirm}>Confirm</a>
+        </div>
       </form>
+      <div className={styles.buttonsignup}>
+        <div className={styles.ask}>New to Raynia? </div>
+        <a href="/sign-up" className={styles.signup}>Create an account</a>
+      </div>
     </div>
   );
 };
